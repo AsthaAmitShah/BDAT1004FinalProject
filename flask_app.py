66f02b98452e030fc0bc0dedab99a1dfa2db04fe
Astha Amit Shah <200515717@student.georgianc.on.ca>
2022-08-09 01:10:33 -0400

    Added first api

---
+++ flask_app.py
@@ -0,0 +1,78 @@
+from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+import requests
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="BDATProject",
+    password="myExchangeRates",
+    hostname="BDATProject.mysql.pythonanywhere-services.com",
+    databasename="BDATProject$ExchangeRates",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+API_KEY = "P2dnLPUw5Pz0HyGerxzFhaJLb89sASMq"
+
+class CurrenySymbols(db.Models):
+    __tablename__ = "currencySymbols"
+    curr = db.Column(db.String(5))
+    currName = db.Column(db.String(50))
+
+class ExchangeRateRow(db.Model):
+    __tablename__ = "exchangeRates"
+
+    date = db.Column(db.Integer, primary_key=True)
+    lastUpdated = db.Column(db.Integer)
+    USD = db.Column(db.Float64)
+    CAD = db.Column(db.Float64)
+    INR = db.Column(db.Float64)
+    EUR = db.Column(db.Float64)
+    AED = db.Column(db.Float64)
+    BHD = db.Column(db.Float64)
+    HKD = db.Column(db.Float64)
+    JPY = db.Column(db.Float64)
+    Highest = db.Column(db.String(5))
+    HighestValue = db.Column(db.Float64)
+    Lowest = db.Column(db.String(5))
+    LowestValue = db.Column(db.Float64)
+
+@app.route("/initialize", methods=["GET"])
+def initialize():
+
+    if len(CurrenySymbols.query.all()) == 0:
+        url = "https://api.apilayer.com/exchangerates_data/symbols"
+
+        payload = {}
+        headers= {
+        "apikey": API_KEY
+        }
+
+        result = {}
+        try:
+            response = requests.get(url, headers=headers, data = payload)
+            if response.status_code == 200:
+                result = response.json()
+            else:
+                return("The api throwed the following error:", response.status_code)
+
+        except requests.exceptions.HTTPError as e:
+                return("The code encountered the following HTTPError error:", e)
+        except requests.exceptions.RequestException as e:
+            return("The code encountered the following RequestException error:", e)
+        except Exception as e:
+            return("The code encountered the following error:", e)
+
+        for curr,currencyName in result.get("symbols", {}).items():
+            currObj = CurrenySymbols(curr=curr,currName=currencyName)
+            db.session.add(currObj)
+        
+        db.session.commit()
+        
+
+    return
